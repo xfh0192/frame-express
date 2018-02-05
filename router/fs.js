1a74ae6ee26631ffeb4bdf38155524f5b0a1af04
xfh <461144823@qq.com>
2018-02-05 20:08:28 +0800

    add fs delete and clearDir

--- router/fs.js
+++ router/fs.js
@@ -25,7 +25,7 @@ router.post('/file/set', async (req, res) => {
         if (err) throw err;
         console.log('文件写入：' + './' + fileName)
 
-        let fileList = await getsFileList()
+        let fileList = await getFileList()
         let list = fileList.map((item, index) => {
             return {key: index, label: item, disabled: false}
         })
@@ -61,7 +61,7 @@ router.get('/file/list/get', async (req, res) => {
     // let dirName = params.dirName || '../fileStore/';
     // let dirName = `${config.fileStorePath}/${params.dirName || ''}/`;
     
-     let fileList = await getFileList(params.dirName)
+    let fileList = await getFileList(params.dirName)
     // 处理格式
     /**
      * 返回的 fileList = [fileName1, fileName2]...
@@ -81,13 +81,55 @@ router.get('/file/list/get', async (req, res) => {
     res.send(data);
 })
 
+// 删除指定的文件
+router.post('/file/remove', async (req, res) => {
+    let params = req.body || {}
+
+    try {
+        let response = await removeFile(params.fileName);
+        console.log(response)
+    } catch (err) {
+        console.log(err)
+        return;
+    }
+
+    let fileList = await getFileList()
+    let list = fileList.map((item, index) => {
+        return {key: index, label: item, disabled: false}
+    })
+    let data = {
+        count: list.length,
+        rows: list,
+    }
+    res.send(data);
+})
+
+router.post('/file/dir/clear', async (req, res) => {
+    try {
+        await clearDir()
+    } catch (err) {
+        console.log(err)
+        return;
+    }
+
+    let fileList = await getFileList()
+    let list = fileList.map((item, index) => {
+        return {key: index, label: item, disabled: false}
+    })
+    let data = {
+        count: list.length,
+        rows: list,
+    }
+    res.send(data);
+})
+
 module.exports = router;
 
 
 
 // ---------------------------------------------
 // 公共逻辑
-// fs模块中的方法好像没有返回promise，所以
+// fs模块中的方法好像没有返回promise，所以用promise包装一下。出错目前就直接抛出先，因为暂时不考虑阻止的情况
 
 // 获取某文件夹中的文件名列表
 // 暂时利用函数变量提升特性，后面重构进独立模块
@@ -104,3 +146,51 @@ async function getFileList (dirName) {
         })
     })
 }
+
+// 检查某文件是否文件夹
+async function isDirectory (fileName) {
+    return await new Promise((resolve, reject) => {
+        fs.stat(fileName, (err, stats) => {
+            if (err) {
+                throw new Error(`打开文件失败：isDirectory`)
+            }
+            resolve(stats.isDirectory());
+        })
+    })
+}
+
+// 删除某文件
+async function removeFile (fileName) {
+    return await new Promise((resolve, reject) => {
+        fs.unlink(`${config.fileStorePath}/${fileName}.txt`, (err) => {
+            if (err) {
+                reject(`删除文件出错：${fileName} 该文件不存在`)
+            }
+            resolve('删除文件夹内容完成')
+        })
+    })
+}
+
+// 清空某文件夹
+async function clearDir (dirName) {
+    let checkDirectory = await isDirectory(`${config.fileStorePath}`);
+    let fileList = await getFileList();     // 获取目录中的文件列表
+
+    if (!checkDirectory) {
+        throw new Error('目标位置不是文件夹')
+    }
+    for (let file of fileList) {
+        await fs.unlink(`${config.fileStorePath}/${file}`, (err) => {
+            if (err) {
+                throw new Error(`清除目录出错`)
+            }
+            console.log(`清除文件：${file}.txt`)
+        })
+    }
+
+    console.log(`清除目录完成`)
+    return await Promise.resolve(`清除目录完成`);
+}
+
+// 删除某目录
+// fs.rmdir()
